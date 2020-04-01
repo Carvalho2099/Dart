3459a82535928a50f29706ce65210f2959e4c05a
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-01 17:33:36 -0400

    variaveis 1

---
+++ Fundamentos/variaveis_1.dart
@@ -0,0 +1,8 @@
+main(){
+  int a = 2; // Tipo, nome, atribuição.
+  double b = 3.1314; // Tipo, nome, atribuição.
+  b = 3.1415; // atribuindo novo valor a variavel.
+
+  print(a); //mostrando valores.
+  print(b);
+}
