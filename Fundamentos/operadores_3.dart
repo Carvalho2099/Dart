ac1b3741f2e8466e6d44606a84a1d76cd898a4c6
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-02 10:20:29 -0400

    operadores 3

---
+++ Fundamentos/operadores_3.dart
@@ -0,0 +1,19 @@
+main(){
+  //Operadores unarios
+  int a = 3;
+  a = a + 1;
+  a += 1;
+  a++;
+  a = a - 1;
+  a -= 1;
+  a--;
+
+  //Operador unario (NOT)
+  print(!true);
+  print(!false);
+
+  bool x = false;
+  print(!x);
+
+
+}
