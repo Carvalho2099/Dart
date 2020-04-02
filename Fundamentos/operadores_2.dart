9d4de9c03b7660cd93da680d549f0432dc30c5e6
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-02 10:09:32 -0400

    operadores 2

---
+++ Fundamentos/operadores_2.dart
@@ -0,0 +1,19 @@
+main(){
+  //Atribuição
+  int a = 2;
+  a = a + 3;
+  a += 3; //a = a + 3
+  a -= 3; //a = a - 3
+  a *= 3; //a = a * 3
+
+  //Relacionais - O resultado sempre é bool
+
+  print(3 > 2);
+  print(3 >= 2);
+  print(3 < 4);
+  print(3 <= 3);
+  print(3 != 3);
+  print(3 == 3);
+
+
+}
