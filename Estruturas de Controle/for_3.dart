6212e7b78865e78aa2b7b18aff092bee48eca857
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-03 18:25:27 -0400

    for 3

---
+++ Estruturas de Controle/for_3.dart
@@ -0,0 +1,26 @@
+main(){
+  Map<String, double> notas = {
+    'João Pedro': 9.1,
+    'Mara Augusta': 7.2,
+    'Ana Silva': 6.4,
+    'Roberto Andrade': 8.8,
+    'Pedro Firmino': 9.9,
+
+  };
+
+  for(String nome in notas.keys){
+    print("Nome do aluno é $nome");
+  }
+
+  for(String nome in notas.keys){
+    print("Nome do aluno é $nome e a nota é ${notas[nome]}.");
+  }
+
+  for(var nota in notas.values){
+    print("A nota é $nota");
+  }
+
+  for(var registro in notas.entries){
+    print("O ${registro.key} tem nota ${registro.value}.");
+  }
+}
