d48fd77ea4479c0007fbbc92ddfc49f190ce50c4
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-05 17:15:55 -0400

    filter 3

---
+++ Funções/filter_3.dart
@@ -0,0 +1,23 @@
+List<E> filtrar<E>(List<E> lista, bool Function(E) fn){
+  List<E> listaFiltrada = [];
+  for(E elemento in lista){
+    if(fn(elemento)){
+      listaFiltrada.add(elemento);
+    }
+  }
+  return listaFiltrada;
+}
+
+main(){
+  var notas = [8.2, 7.3, 5.8, 5.4, 2.7, 9.3];
+  var boasNotasFn = (double nota) => nota >= 7.5;
+
+  var somenteNotasBoas = filtrar(notas, boasNotasFn);
+  print(somenteNotasBoas);
+
+
+  var nomes = ['Ana', 'Bia', 'Rebeca', 'Gui', 'João'];
+  var nomesGrandeFn = (String nome) => nome.length >= 5;
+
+  print(filtrar(nomes, nomesGrandeFn));
+}
