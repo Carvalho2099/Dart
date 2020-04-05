10adbc5817ebc22030221703e1bde96d1b92bb7f
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-05 10:11:40 -0400

    retornar função

---
+++ Funções/retornar_funcao.dart
@@ -0,0 +1,15 @@
+int Function(int) somaParcial(int a){
+  return (int b){
+    return a + b;
+  };
+}
+
+main(){
+  print(somaParcial(2)(10));
+
+  var somaCom10 = somaParcial(10);
+
+  print(somaCom10(3));
+  print(somaCom10(7));
+  print(somaCom10(19));
+}
