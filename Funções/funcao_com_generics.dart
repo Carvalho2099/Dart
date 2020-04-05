0e6c59f1930a5bf100c7a7167e162f6c4e44f117
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-05 10:22:19 -0400

    função com Generics

---
+++ Funções/funcao_com_generics.dart
@@ -0,0 +1,17 @@
+Object segundoElementoV1(List lista){
+  return lista.length >= 2 ? lista[1] : null;
+}
+E segundoElementoV2<E>(List<E> lista){
+  return lista.length >= 2 ? lista[1] : null;
+}
+main(){
+  var lista = [3, 6, 7, 12, 45, 78, 1];
+  print(lista);
+  print(segundoElementoV1(lista));
+  
+  //print(segundoElementoV2<String>(lista));
+  int segundoElemento = segundoElementoV2<int>(lista);
+  print(segundoElemento);
+  segundoElemento = segundoElementoV2(lista);
+  print(segundoElemento);
+}
