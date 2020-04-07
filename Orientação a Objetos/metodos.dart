ede3fbbf02b7f6a52396d458aecb34f435d5a23f
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-07 08:42:00 -0400

    metodos

---
+++ Orientação a Objetos/metodos.dart
@@ -0,0 +1,29 @@
+class Data{
+  int dia;
+  int mes;
+  int ano;
+  String obterDataFormatada(){
+    return "$dia/$mes/$ano";
+  }
+}
+
+main(){
+  var dataAniversario = new Data();
+  dataAniversario.dia = 3;
+  dataAniversario.mes = 10;
+  dataAniversario.ano = 2020;
+
+  Data dataCompra = Data();
+  dataCompra.dia = 23;
+  dataCompra.mes = 12;
+  dataCompra.ano = 2021; 
+ 
+  //print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  //print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  String d1 = dataAniversario.obterDataFormatada();
+  String d2 = dataCompra.obterDataFormatada();
+
+  print("A data de aniversário é $d1");
+  print("A data da compra é ${dataCompra.obterDataFormatada()}");
+
+}
