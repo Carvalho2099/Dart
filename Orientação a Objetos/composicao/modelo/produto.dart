8ac972caa29ebd5f2c3843ab9e2c5e81a1fe03c2
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-07 17:32:03 -0400

    composição 1

---
+++ Orientação a Objetos/composicao/modelo/produto.dart
@@ -0,0 +1,13 @@
+class Produto{
+  int codigo;
+  String nome;
+  double preco;
+  double desconto;
+
+  Produto({this.codigo, this.nome, this.preco, this.desconto = 0});
+
+  double get precoComDesconto{
+    return (1 - desconto)* preco;
+  }
+
+}
